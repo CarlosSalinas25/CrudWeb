0e04fcec4ee382e60d3fdb58615aaa9627654c81
CarlosSalinas25 <20210290@ricaldone.edu.sv>
2025-05-30 12:08:15 -0600

    CRUDWEB

---
+++ Js/Script.js
@@ -0,0 +1,47 @@
+//Endpoint de intregrantes - API
+const API_URL = "https://retoolapi.dev/QQN2jw/Integrantes"; 
+
+//Función que manda a traer el JSON con GET
+async function ObtenerIntegrantes() {
+
+    //Respuesta de el servidor
+    const respuesta = await fetch(API_URL);
+    
+    //Pasamos a JSON la respuesta del servidor
+    const data = await respuesta.json(); //Esto es un JSON
+
+    //Enviamos el JSON a la función que genera las filas en la tabla
+    MostrarDatos(data);
+    
+}
+
+
+
+//Función para crear las filas de la tabla en base a un JSON
+//"datos" representará al JSON donde viene la información
+function MostrarDatos(datos){
+
+    //Se llama a la tabla con elemento "id" y luego al tbody
+    const tabla = document.querySelector("#tabla tbody");
+
+    //Para inyectar código HTML usamos "innerHTML"
+    tabla.innerHTML = ""; //Vaciamos el contenido de la tabla
+
+    datos.forEach(integrante => {
+        tabla.innerHTML += `
+            <tr>
+                <td>${integrante.id}</td>
+                <td>${integrante.Nombre}</td>
+                <td>${integrante.Apellido}</td>
+                <td>${integrante.Correo}</td>
+                <td>
+                    <button>Editar</button>
+                    <button>Eliminar</button>
+                </td>
+            </tr>
+        `;
+    });
+}
+
+ObtenerIntegrantes();
+
